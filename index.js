e123c2ed23fdee6e080819f63c1a9128412fd58b
BattlesheepQJR <Jacob@boydfamily.org.uk>
2025-08-19 15:06:44 +0100

    Update index.js
    Signed-off-by: BattlesheepQJR <Jacob@boydfamily.org.uk>

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const pass = document.getElementById(`password`)
-const login = document.getElementById(`Login`)
+const pass = document.getElementById('password').value
+const login = document.getElementById('Login')
+
+login.addEventListener('login', (event) => {
+  event.preventDefault();
+  if(pass === $password) {
+    document.getElementById('password').hidden = true
+    document.getElementById('username').hidden = true
+  }
+}
